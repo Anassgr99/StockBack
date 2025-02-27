a5776a919fdb23851cb04c5c4e6deed9801b6841
anass gr <80433741+Anassgr99@users.noreply.github.com>
2025-02-27 12:23:29 +0000

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,19 +1,16 @@
 import mysql from 'mysql2';
 import dotenv from 'dotenv';
-import url from 'url';
 
 dotenv.config(); // Load environment variables from .env file
 
-// Parse the MySQL URL
-const mysqlUrl = url.parse(process.env.MYSQL_URL);
-const [user, password] = mysqlUrl.auth.split(':');
-const host = mysqlUrl.hostname;
-const port = mysqlUrl.port;
-const database = mysqlUrl.pathname.split('/')[1];
-mysqlUrl='mysql://root:YpvkUbEqjDSkeZQwVOByggVHjZqVVCDB@mysql.railway.internal:3306/railway';
-
 // MySQL Database Connection
-const db = mysql.createConnection(mysqlUrl);x
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+});
 
 db.connect((err) => {
     if (err) {
